9b9edd1032efd84e224764ca896ed6cb4759fcab
Diego Araque <diegoaraquej@gmail.com>
2022-10-13 06:31:23 -0500

    add new changes jenkinsfile

--- main.tf
+++ main.tf
@@ -1,6 +1,18 @@
 provider "aws" {
 	region = "us-east-2"
 }
+
+resource "aws_instance" "ec2Instance" {
+	ami = "ami-097a2df4ac947655f"
+	instance_type = "t2.micro"
+
+    tags = {
+		Name = "EC2 Terraform Onboarding1"
+	}
+    vpc_security_group_ids = [aws_security_group.sgOnboarding.id]
+
+}
+
 resource "aws_security_group" "sgOnboarding" {
 	name = "terraform-tcp-sg-onboarding"
 	
