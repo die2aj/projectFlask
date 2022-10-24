82e914e171029f761f5282938a12e52a4ae05c72
Diego Araque <diegoaraquej@gmail.com>
2022-10-24 08:32:55 -0500

    add new changes

--- Jenkinsfile
+++ Jenkinsfile
@@ -5,8 +5,18 @@ pipeline {
     }
   environment {
     DOCKERHUB_CREDENTIALS = credentials('diegoaco-dockerhub')
+    AWS_ACCESS_KEY_ID=credentials('diegoa-aws-cred')
+    AWS_SECRET_ACCESS_KEY=credentials('diegoa-aws-cred')
   }
   stages {
+
+  stage('Ansible installation'){
+            steps{
+                sh 'ansible --version'
+
+           }
+        }
+
     stage('Build') {
       steps {
         sh 'docker build -t diegoaco/project_flask .'
@@ -32,10 +42,27 @@ pipeline {
                 sh 'terraform apply --auto-approve'
             }
         }
+
+         stage('Ansible') {
+              steps {
+                dir('ansible') {
+                    ansiblePlaybook(
+                        credentialsId: 'diegoKeyPair2',
+                        disableHostKeyChecking: true,
+                        installation: 'ansible',
+                        inventory: 'hosts.inv',
+                        playbook: 'test.yml'
+                    )
+                }
+             }
+        }
   } 
   post {
     always {
       sh 'docker logout'
     }
   }
+
+  
+
 }
--- main.tf
+++ main.tf
@@ -3,15 +3,9 @@ provider "aws" {
 }
 
 resource "aws_instance" "ec2Instance" {
-	ami = "ami-097a2df4ac947655f"
+	ami = "ami-089a545a9ed9893b6"
 	instance_type = "t2.micro"
-
-	user_data = <<-EOF
-  	#!/bin/bash
-  	sudo apt update -y
-  	sudo snap install docker
-
-  	EOF
+  key_name = "diegoKeyPair2"
 
     tags = {
 		Name = "EC2 Terraform Onboarding1"
@@ -19,6 +13,24 @@ resource "aws_instance" "ec2Instance" {
     vpc_security_group_ids = [aws_security_group.sgOnboarding.id]
 
 }
+
+resource "aws_eip" "ec2lasticeip" {
+
+  instance = aws_instance.ec2Instance.id
+
+  vpc = true
+
+}
+
+/* output "instance_id" {
+  description = "ID of the EC2 instance"
+  value       = aws_instance.ec2Instance.id
+}
+
+output "instance_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.ec2Instance.public_ip
+} */
 
 resource "aws_security_group" "sgOnboarding" {
 	name = "terraform-tcp-sg-onboarding"
@@ -48,4 +60,12 @@ resource "aws_security_group" "sgOnboarding" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
+  
+}
+resource "local_file" "tf_variables_ansible" {
+  content = <<-DOC
+    [ec2Instance] 
+    ${aws_eip.ec2lasticeip.public_ip}
+    DOC
+  filename = "ansible/hosts.inv"
 }
